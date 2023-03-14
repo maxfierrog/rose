b6690be551fe305b2418178f51203132c92b5d7a
Max Fierro <maxfierro@berkeley.edu>
2023-03-14 00:23:58 -0700

    added clap flow

--- src/main.rs
+++ src/main.rs
@@ -10,72 +10,78 @@ pub mod help;
 pub mod add;
 
 
-use std::env;
-use core::panic;
-use clap::Parser;
+use clap::{Args, Parser, Subcommand};
 
 
-#[derive(Parser, Debug)]
-struct Args {
-   #[arg(short, long)]
-   source: String,
-   
-   #[arg(short, long)]
-   destination: String,
-
-   #[arg(short, long)]
-   canon: String,
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    /// Send no output to STDOUT during execution
+    #[arg(short, long)]
+    quiet: bool,
+    /// Provide extra execution information to STDOUT
+    #[arg(short, long)]
+    verbose: bool,
+    /// Skips prompts for destructive operations
+    #[arg(short, long)]
+    yes: bool,
+    #[command(subcommand)]
+    command: Commands
 }
 
 
+#[derive(Subcommand)]
+enum Commands {
+    /// Create a documentation tree with many individual files
+    Doctree(MakeTree),
+    /// Create a single documentation file
+    Docfile(MakeFile),
+    /// Creates a default JSON doctree and docfile specifier in target
+    Init(Init)
+}
 
-/* GLOBAL VALUES */
+#[derive(Args)]
+struct MakeTree {
+    /// If provided, generates doctree based on project's source code
+    #[arg(short, long)]
+    project: Option<String>,
 
-/* Command names (should be in plaintext). */
-const COMMANDS: [&str; 4] = ["doctree", "docfile", "add", "remove"];
+    /// Destination directory for documentation tree
+    #[arg(short, long)]
+    dest: String
+}
 
-/* Full argument options (should be in the format "--{}="). */
-const LONG_OPTIONS: [&str; 3] = ["source", "dest", "canon"];
+#[derive(Args)]
+struct MakeFile {
+    /// If provided, generates document based on file's source code
+    #[arg(short, long)]
+    source: Option<String>,
 
-/* Shorthands for argument options (should be in the format "-{}="). */
-const SHORT_OPTIONS: [&str; 3] = ["s", "d", "c"];
+    /// Destination directory for document
+    #[arg(short, long)]
+    dest: String
+}
 
-/* Full flags (should be prefixed with "--"). */
-const LONG_FLAGS: [&str; 3] = ["quiet", "verbose", "yes"];
+#[derive(Args)]
+struct Init {
+    /// Destination directory for JSON architecture spec
+    #[arg(short, long)]
+    target: String
+}
 
-/* Shorthands for flags (should be prefixed with "-"). */
-const SHORT_FLAGS: [&str; 3] = ["q", "v", "y"];
 
-
-/* ENTRY */
-
-/* Main I/O loop. There are two modes for this program: 1) standard UNIX CLI
-command style, where you pass in a command with options and arguments, and 2)
-an interactive full-screen interface for ease of use. The interactive mode
-is activated when only 'rose' is executed. Otherwise, we assume that the user
-wanted to execute a UNIX style command, and display help and whatnot. */
 fn main() {
-    let args: Vec<_> = env::args().collect();
-    let mode: bool = args.len() == 1;
-    if mode {
-        clear_screen();
-        loop {
-            action_menu();
-            clear_screen();
-            if finished_actions() { break; }
-        };
-    } else {
-        let args = Args::parse();
-        handle_arguments(args);
+    let cli = Cli::parse();
+    match &cli.command {
+        Commands::Doctree(MakeTree) => {
+
+        },
+        Commands::Docfile(MakeFile) => {
+
+        },
+        Commands::Init(Init) => {
+
+        }
     }
-}
-
-
-/* COMMAND MODE ARGUMENT HANDLING */
-
-/* Accepts the arguments handed to the program and executes the appropriate
-action without printing anything, or exits with the correct error code should
-something go wrong in the process (also without printing anything). */
-fn handle_arguments(args: Args) {
-
 }
